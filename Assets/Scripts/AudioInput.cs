ea47ddfbf5d7f75dc6b12700e44082b0e5fa9ffe
Draguve <draguve@gmail.com>
2019-09-28 02:04:44 +0530

    Queue and sampling stuff

--- Assets/Scripts/AudioInput.cs
+++ Assets/Scripts/AudioInput.cs
@@ -7,28 +7,41 @@ using Lasp;
 
 public class AudioInput : MonoBehaviour
 {
+    public int size = 4096; // more than 1024
+    public int sampFreq = 22050;
     
-    Complex[] freq = new Complex[2048];
-    private float[] samples = new float[1024];
-    private float[] lastsamples = new float[1024];
+    
+    Complex[] freq;
+    private float[] samples= new float[1024];
+    private float[] lastsamples;
+    private int fakeSize;
+    private float freqStep;
+    
     
     // Start is called before the first frame update
     void Start()
     {
-        for(int i = 0; i < lastsamples.Length; i++)
-        {
-            lastsamples[i] = 0;
-        }
+        
+        freq = new Complex[size];
+        fakeSize = size - 1024;
+        lastsamples = new float[fakeSize];
+        freqStep = sampFreq / (size/2);
     }
 
     // Update is called once per frame
     void Update()
     {
-        for(int i = 0; i < lastsamples.Length; i++)
+
+        for (int i = 1024,j=0; i < fakeSize; i++,j++)
         {
-            lastsamples[i] = samples[i];
+            lastsamples[j] = lastsamples[i];
         }
-        
+        for (int i=0,j=fakeSize-1024;j<fakeSize; i++,j++)
+        {
+            lastsamples[j] = samples[i];
+        }
+
+
         MasterInput.RetrieveWaveform(FilterType.Bypass,samples);
         
         for(int i = 0; i < lastsamples.Length; i++)
@@ -37,7 +50,7 @@ public class AudioInput : MonoBehaviour
         }
         for(int i = 0; i < samples.Length; i++)
         {
-            freq[1024 + i] = new Complex(samples[i],0);
+            freq[lastsamples.Length + i] = new Complex(samples[i],0);
         }
         
         for (int i = 0; i < samples.Length; i++)
@@ -50,7 +63,7 @@ public class AudioInput : MonoBehaviour
         for (int i = 0; i < freq.Length/2; i++) // plot only the first half
         {
             // multiply the magnitude of each value by 2
-            Debug.DrawLine(new Vector3(i, 4), new Vector3(i, 20+(float)freq[i].magnitude * 200), Color.white);
+            Debug.DrawLine(new Vector3(i, 200), new Vector3(i,  200  + (float)freq[i].magnitude * 200), Color.white);
         }
     }
 }
