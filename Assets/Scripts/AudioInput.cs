edb6efb40ee817b7dfeb31a9e63dd31538ad5c1a
Draguve <draguve@gmail.com>
2019-09-28 15:50:42 +0530

    random colors

--- Assets/Scripts/AudioInput.cs
+++ Assets/Scripts/AudioInput.cs
@@ -4,15 +4,17 @@ using System.Collections.Generic;
 using UnityEngine;
 using B83.MathHelpers;
 using Lasp;
+using ProceduralToolkit;
 
 
 public class AudioInput : MonoBehaviour
 {
+    public ParticleSystem objectToColorChange;
     public int size = 4096; // more than 1024
     public int sampFreq = 22050;
     public int minNote =  40;
     public int maxNote = 95;
-     
+    public float noteThreshold = 0f;
     
     Complex[] freq;
     private float[] samples= new float[1024];
@@ -20,7 +22,7 @@ public class AudioInput : MonoBehaviour
     private int fakeSize;
     private float freqStep;
     private string[] NOTES;
-
+    private ColorHSV[] Colors;
     private int imin, imax;
     
     
@@ -32,7 +34,14 @@ public class AudioInput : MonoBehaviour
         fakeSize = size - 1024;
         lastsamples = new float[fakeSize];
         freqStep = (float)sampFreq / (size/2f);
-        NOTES = "C C# D D# E F F# G G# A A# B".Split('a');
+        NOTES = "C C# D D# E F F# G G# A A# B".Split(' ');
+        Colors = new ColorHSV[12];
+        Colors[0] = new ColorHSV(Color.red);
+        for (int i = 1; i < 12; i++)
+        {
+            Colors[i] = Colors[0].WithH(i*(360f/48f));
+            Debug.Log(Colors[i].ToString());
+        }
         imin = (int)Mathf.Max(0, Mathf.Floor(note_to_fftbin(minNote - 1)));
         imax = (int)Mathf.Min(size/2, (Mathf.Ceil(note_to_fftbin(maxNote + 1))));
 
@@ -76,18 +85,48 @@ public class AudioInput : MonoBehaviour
             Debug.DrawLine(new Vector3(i, 200), new Vector3(i,  200  + (float)freq[i].magnitude * 200), Color.white);
         }
 
-        int maxHeight = imin;
-        float max = freq[imin].fMagnitude;
-
+        List<FreqData> maximas = new List<FreqData>();
+        
         for (int i = imin; i < imax; i++)
         {
-            if (max < freq[i].fMagnitude)
+            if (freq[i].magnitude > noteThreshold)
             {
-                maxHeight = i;
-                max = freq[i].fMagnitude;
+                if (freq[i].magnitude > freq[i - 1].magnitude && freq[i].magnitude > freq[i + 1].magnitude)
+                {
+                    FreqData temp = new FreqData();
+                    temp.location = i;
+                    temp.mag = freq[i].magnitude;
+                    maximas.Add(temp);
+                }
             }
         }
-        Debug.Log((maxHeight*freqStep));
+
+        if (maximas.Count > 0)
+        {
+            maximas.Sort((s1, s2) => s1.mag.CompareTo(s2.mag));
+            float maxFreq = maximas[0].location * freqStep;
+            int closest = (int) FreqToNumber(maxFreq);
+            float closestFreq = NumberToFreq(closest);
+            ColorHSV colorA = Colors[closest % 12],colorB;
+            if (maxFreq - closestFreq > 0)
+            {
+                colorB = Colors[(closest + 1) % 12];
+            }
+            else
+            {
+                colorB = Colors[(closest - 1) % 12];
+            }
+
+            var mainModule = objectToColorChange.main;
+            Color x =  ColorHSV.Lerp(colorA, colorB, Mathf.Abs(maxFreq - closestFreq) / maxFreq).ToColor();
+            mainModule.startColor = x;
+        }
+    }
+
+    struct FreqData
+    {
+        public int location;
+        public double mag;
     }
 
 
